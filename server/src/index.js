6757728eb6a031684a29317a8e682323ba8735c1
isidoranedic <isidora.nedic@mail.utoronto.ca>
2025-05-16 22:15:50 -0400

    uncommented cors line in server

--- server/src/index.js
+++ server/src/index.js
@@ -11,10 +11,10 @@ const PORT = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
 const SECRET_KEY = process.env.SECRET_KEY; // for JWT tokens
 
-//const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 const cors = require("cors");
 app.use(cors({
-    //origin: FRONTEND_URL,
+    origin: FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
