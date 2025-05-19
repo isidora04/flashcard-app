860cc7219928ff5ed6d04802bf79cb059865f96a
isidora04 <144294518+isidora04@users.noreply.github.com>
2025-05-19 18:51:03 -0400

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -808,7 +808,7 @@ app.delete("/flashcards/:setId/:cardId", jwtAuth, ownerAuth, async (req, res) =>
     }
 });
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
 
